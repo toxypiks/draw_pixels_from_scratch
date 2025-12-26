623d246a7295ff783deeaa788d94c23da9eef2ea
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-26 17:01:20 +0100

    Draw circle checkboard

--- src/main.c
+++ src/main.c
@@ -6,6 +6,24 @@
 #include <math.h>
 #include "draw_pixel.h"
 
+void fill_circle_checkboard(PixelBuf *pb, int cols, int rows, uint32_t color)
+{
+    int cell_width = pb->width/cols;
+    int cell_height = pb->height/rows;
+
+    fill_pixels(pb, 0xFF202020);
+
+    for(int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            int radius = cell_width;
+            if (cell_height < radius) {
+                radius = cell_height;
+            }
+            fill_circle(pb, x*cell_width + cell_width/2, y*cell_height + cell_height/2, radius/2, color);
+        }
+    }
+}
+
 int main(void)
 {
     printf("Hello from C\n");
@@ -16,12 +34,9 @@ int main(void)
         return 1;
     }
 
-    //fill_pixels(my_pb, 0xFF202020);
-    //fill_rectangle(my_pb, my_pb->width/2 - 50, my_pb->height/2 -50, 100, 100, 0xFFFF0000);
+    //call your drawing function you wanna test here
+    fill_circle_checkboard(my_pb, 8, 8, 0xFF00FF00);
 
-    //fill_rectangle(my_pb, my_pb->width/2 - 100, my_pb->height/2 -100, 100, 100, 0xFF00FF00);
-
-    fill_circle(my_pb, my_pb->width/2, my_pb->height/2, 100, 0xFF00FF00);
     char *file_path = "output.ppm";
     Errno err = save_to_ppm_file(my_pb, file_path);
     if (err) {
