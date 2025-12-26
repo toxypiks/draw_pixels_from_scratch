f0f0147f9230cd68bf1899b6db69af61ed1946a1
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-26 10:12:35 +0100

    Add more exercises

--- src/2d_thinking_exercises.c
+++ src/2d_thinking_exercises.c
@@ -1,32 +1,113 @@
 #include <stdio.h>
 
+void print_chessboard(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if ((x + y) % 2 == 0) {
+                printf("*");
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void print_sum_grid(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            printf("%d ", x + y);
+        }
+        printf("\n");
+    }
+}
+
+void print_rect(int width, int height)
+{
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void print_diagonal(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (y == x) {
+                printf("X");
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void fill_array(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            printf("%d ", y*width + x);
+        }
+        printf("\n");
+    }
+}
+
+void fill_cross(int width, int height)
+{
+    int mid_x = width/2;
+    int mid_y = height/2;
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (y == mid_y || x == mid_x) {
+                printf("X");
+            }
+            else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void fill_diagonal_cross(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (y == x || x + y == width -1) {
+                printf("X");
+            }
+            else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void fill_frame(int width, int height)
+{
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            if (x == 0 || y == 0 || x == width -1 || y == height -1) {
+                printf("X");
+            }
+            else {
+                printf(".");
+            }
+        }
+        printf("\n");
+    }
+}
+
 int main(void)
 {
-    printf("Hello from excercises\n");
-
-    void print_chessboard(int width, int height)
-    {
-        for(int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                if ((x + y) % 2 == 0) {
-                    printf("*");
-                } else {
-                    printf(".");
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    void print_sum_grid(int width, int height)
-    {
-        for(int y = 0; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                printf("%d ", x + y);
-            }
-            printf("\n");
-        }
-    }
+    fill_frame(7 ,7);
 
     return 0;
 }
